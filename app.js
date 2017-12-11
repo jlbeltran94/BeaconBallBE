2b96363fa26619770c8cc76e7e07398f0ce45a2f
Felipe Arcos <pipearcos221@gmail.com>
2017-12-10 20:52:44 -0500

    se creo el backend para usuarios, registro y logueo

--- app.js
+++ app.js
@@ -4,19 +4,37 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongodb = require('mongodb').MongoClient;
+var cors =  require("cors");
 
-var index = require('./routes/index');
 var users = require('./routes/users');
+
+
 
 var app = express();
 
+let dbConnection;
+mongodb.connect("mongodb://localhost:27017/beaconball", (err, db) => {
+
+  if (err) {
+    console.log("Error:" + err);
+  } else {
+    console.log("Mongo Conectado");
+    dbConnection = db;
+  }
+
+});
+
+app.use((req, res, next) => {
+
+  req.db = dbConnection;
+  next();
+
+});
+
+app.use(cors());
 
 
-
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,51 @@
+import { read } from 'fs';
+
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.use((req, res, next) => {
+  req.collection = req.db.collection("users");
+  next;
 });
 
-// router.post('msg', {
+// REGISTRARSE
+
+router.post("/signin", (req, res, next) => {
   
-// })
+  let user = req.body;
+
+  req.collection.findOne({ username: user.username }).then(doc => {
+    if(doc) {
+      res.send({ success: false, exist: true });
+    } else{
+      req.collection.insert(user).then(result => {
+        res.send({ success: true });
+      }).catch(err => {
+        res.send({ success:false });
+      });
+    }
+  }).catch(err => {
+    res.send({ success:false });
+  });
+
+});
+
+// LOGUEARSE
+
+router.post("/login", (req, res, next) => {
+
+  let body = req.body;
+  
+  req.collection.findOne({ username: body.username, password: body.password }).then(doc => {
+    if(doc){
+      res.send({ success: true, user:doc });
+    }else {
+      res.send({ success: false });
+    }
+  }).catch(err => {
+    res.send({ success: false });
+  });
+
+});
 
 module.exports = router;
